4c48470b17fa15af8aeb942a971c6814db49fb1d
Marcel <marcel.zanluca@gmail.com>
2015-05-02 14:35:43 -0300

    MZ: Hotsite para o varejo gratuito.

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    toastr.options = { "positionClass": "toast-bottom-right" };
+
+    // âncoragem
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+    // Cadastrando o contato
+    $(".form-inline").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "para qual url?",
+            data: {
+                nome: $('#inputNome').val(),
+                email: $('#inputEmail').val(),
+                telefone: $('#inputTelefone').val()
+            },
+            success: function () {
+                toastr.success('Obrigado, recebemos seus dados.');
+            }
+        });
+    });
+});
